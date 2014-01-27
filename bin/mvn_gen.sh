62727ff5e4984c83e68bbe666c786d087ce3d973
wuzhong <wuzhong@taobao.com>
2014-01-27 11:38:01 +0800

    up

--- bin/mvn_gen.sh
+++ bin/mvn_gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn archetype:generate  -DarchetypeArtifactId=android-quickstart -DarchetypeGroupId=de.akquinet.android.archetypes
+mvn archetype:generate -DarchetypeArtifactId=android-quickstart -DarchetypeGroupId=de.akquinet.android.archetypes
